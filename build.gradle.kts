632bcb5f7611c19738f859c1e2ab8880d4cd44db
kriztpow <45737457+kriztpow@users.noreply.github.com>
2025-09-10 07:41:25 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "TVBrowser"
 include(":app")
-
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
