25aaf16785fbaf322c418bf134f4dba7a363411c
kriztpow <45737457+kriztpow@users.noreply.github.com>
2025-09-10 07:39:14 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
+pluginManagement {
+    repositories {
+        google()          // <- necesario para Android Gradle Plugin
+        mavenCentral()    // <- necesario para Kotlin y otras dependencias
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TVBrowser"
+include(":app")
+
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
